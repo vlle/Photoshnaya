e7318cc6c5cc009b434d564e857d141690e358c6
artemiikulikov <aapqwhat@gmail.com>
2023-02-05 18:05:41 +0300

    :shirt: update(db-ops): templates for db-ops add

--- db/db_operations.py
+++ db/db_operations.py
@@ -7,3 +7,24 @@ def init_test_data(engine):
     squidward = User(name="Ivan", fullname="Ivan Foobar", telegram_id="1919118841")
     with Session(engine) as session, session.begin():
         session.add(squidward)
+
+def like_photo(engine, user_id: str, photo_id: str):
+    pass
+
+def remove_like_photo(engine, user_id: str, photo_id: str):
+    pass
+
+def register_photo(engine, user_id: str, photo_id: str):
+    pass
+
+def unregister_photo(engine, user_id: str, photo_id: str):
+    pass
+
+def select_contest_photos(engine, user_id: str, photo_id: str):
+    pass
+
+def select_contest_winner(engine, user_id: str, photo_id: str):
+    pass
+
+def select_contest_theme(engine, user_id: str, photo_id: str):
+    pass
--- db/init_db.py
+++ db/init_db.py
@@ -2,10 +2,8 @@ from db_classes import Base
 from sqlalchemy import MetaData, create_engine
 from db_operations import init_test_data
 
-# declarative base class
 engine = create_engine("sqlite+pysqlite:///sqlite3.db", echo=True)
 metadata_obj = MetaData()
 Base.metadata.create_all(engine)
 
-#init_test_data(engine)
 
