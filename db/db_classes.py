10315cce429522f84f30c6fd0305bd0d4d32476d
Worker Corrino <workerco@at-k6.msk.21-school.ru>
2023-02-08 20:03:23 +0300

    :shirt: update(class): add relationship

--- db/db_classes.py
+++ db/db_classes.py
@@ -1,14 +1,30 @@
 from typing import List
 from typing import Optional
 from sqlalchemy.orm import Mapped
-from sqlalchemy import Integer, String, ForeignKey
+from sqlalchemy import Integer, String, ForeignKey, Table, Column
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import mapped_column
+from sqlalchemy.orm import relationship
 
 
 # declarative base class
 class Base(DeclarativeBase):
     pass
+
+groupPhoto = Table(
+    "groupPhoto",
+    Base.metadata,
+    Column("photo_id", ForeignKey("photo.id")),
+    Column("group_id", ForeignKey("group.id")),
+)
+
+groupUser = Table(
+    "groupUser",
+    Base.metadata,
+    Column("user_id", ForeignKey("user.id")),
+    Column("group_id", ForeignKey("group.id")),
+)
+
 
 class User(Base):
     __tablename__ = "user"
@@ -17,6 +33,7 @@ class User(Base):
     name: Mapped[str] = mapped_column(String(30))
     full_name: Mapped[Optional[str]]
     telegram_id: Mapped[str]
+    photos: Mapped[List["Photo"]] = relationship()
 
     def __repr__(self) -> str:
         return f"User(id={self.id!r}, name={self.name!r}, full_name={self.full_name!r}),\
@@ -30,16 +47,6 @@ class Photo(Base):
     likes: Mapped[int]
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"));
 
-    # projects = relationship(
-    #     "Project",
-    #     secondary=Table(
-    #         "employee_project",
-    #         Base.metadata,
-    #         Column("employee_id", Integer, ForeignKey("employee.id"), primary_key=True),
-    #         Column("project_id", Integer, ForeignKey("project.id"), primary_key=True),
-    #     ),
-    #     backref="employees",
-    # )
 
     def __repr__(self) -> str:
         return f"Photo(id={self.id!r}, hashsum={self.hash!r})"
@@ -54,24 +61,3 @@ class Group(Base):
 
     def __repr__(self) -> str:
         return f"Group(id={self.id!r}, name={self.name!r}, telegram_id={self.telegram_id!r}, contest_theme={self.contest_theme})"
-
-class groupPhoto(Base):
-    __tablename__ = "group_photo"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    photo_id: Mapped[int] = mapped_column(ForeignKey("photo.id"));
-    group_id: Mapped[int] = mapped_column(ForeignKey("group.id"));
-
-    def __repr__(self) -> str:
-        return f"GroupPhoto(id={self.id!r}, photo_id={self.photo_id!r}, group_id={self.group_id!r})"
-
-class groupUser(Base):
-    __tablename__ = "group_user"
-
-    id: Mapped[int] = mapped_column(primary_key=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"));
-    group_id: Mapped[int] = mapped_column(ForeignKey("group.id"));
-
-    def __repr__(self) -> str:
-        return f"GroupUser(id={self.id!r}, user_id={self.user_id!r}, group_id={self.group_id!r})"
-
