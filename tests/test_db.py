4bb6333fb6346d344d0f14daf6c193a1ff9d3c99
artemiikulikov <aapqwhat@gmail.com>
2023-04-21 17:42:48 +0300

    :rotating_light: update(style): fix flake8 warning

--- tests/test_db.py
+++ tests/test_db.py
@@ -58,14 +58,15 @@ def registered_user(registered_group, group, user, db):
     register_unit.register_user(m_user, m_group.telegram_id)
     return register_unit
 
+
 @pytest.fixture()
 def create_user(user, group, registered_group):
     user_list = []
 
     def create_user(*args, **kwargs) -> User:
         us = TUser(name="User №"
-                      + str(random.randint(1, 10000)),
-                 i_id=100 + random.randint(1, 2000))
+                   + str(random.randint(1, 10000)),
+                   i_id=100 + random.randint(1, 2000))
 
         m_user = ObjectFactory.build_user(us.name, us.full_name, us.id)
         m_group = ObjectFactory.build_group(group.group_name, group.group_id)
@@ -97,7 +98,9 @@ def test_is_user_registered(registered_user, user, group, db):
     m_group = ObjectFactory.build_group(group.group_name, group.group_id)
 
     register_unit = RegisterDB(db)
-    assert register_unit.find_user_in_group(m_user.telegram_id, m_group.telegram_id) is True
+    assert register_unit.find_user_in_group(
+            m_user.telegram_id,
+            m_group.telegram_id) is True
 
 
 def test_is_user_not_registered(registered_group, user, group, db):
@@ -105,7 +108,9 @@ def test_is_user_not_registered(registered_group, user, group, db):
     m_group = ObjectFactory.build_group(group.group_name, group.group_id)
 
     register_unit = RegisterDB(db)
-    assert register_unit.find_user_in_group(m_user.telegram_id, m_group.telegram_id) is False
+    assert register_unit.find_user_in_group(
+            m_user.telegram_id,
+            m_group.telegram_id) is False
 
 
 def test_is_5_users_registered(create_user, group, db):
@@ -116,7 +121,9 @@ def test_is_5_users_registered(create_user, group, db):
     m_group = ObjectFactory.build_group(group.group_name, group.group_id)
 
     for user in users:
-        assert register_unit.find_user_in_group(user.telegram_id, m_group.telegram_id) is True
+        assert register_unit.find_user_in_group(
+                user.telegram_id,
+                m_group.telegram_id) is True
 
 
 def test_is_5_users_not_registered(create_user, group, db):
@@ -127,7 +134,9 @@ def test_is_5_users_not_registered(create_user, group, db):
     m_group = ObjectFactory.build_group(group.group_name, group.group_id)
 
     for user in users:
-        assert register_unit.find_user_in_group(user.telegram_id, m_group.telegram_id + 110) is False
+        assert register_unit.find_user_in_group(
+                user.telegram_id,
+                m_group.telegram_id + 110) is False
 
 
 def test_is_admin_registered(create_user, group, db):
@@ -137,12 +146,19 @@ def test_is_admin_registered(create_user, group, db):
     register_unit = RegisterDB(db)
     AdminUnit = AdminDB(db)
     m_group = ObjectFactory.build_group(group.group_name, group.group_id)
-    m_user = ObjectFactory.build_user(users[0].name, users[0].name + ' Ivanov', users[0].telegram_id)
+    m_user = ObjectFactory.build_user(
+            users[0].name,
+            users[0].name + ' Ivanov',
+            users[0].telegram_id)
     register_unit.register_admin(m_user, m_group.telegram_id)
 
-    m_user = ObjectFactory.build_user(users[0].name, users[0].name + ' Ivanov', users[0].telegram_id)
-    
-    assert AdminUnit.check_admin(m_user.telegram_id, m_group.telegram_id) is True
+    m_user = ObjectFactory.build_user(
+            users[0].name,
+            users[0].name + ' Ivanov',
+            users[0].telegram_id)
+
+    assert AdminUnit.check_admin(
+            m_user.telegram_id, m_group.telegram_id) is True
 
 
 def test_is_admin_not_registered(create_user, group, db):
@@ -151,9 +167,13 @@ def test_is_admin_not_registered(create_user, group, db):
         users.append(create_user())
     AdminUnit = AdminDB(db)
     m_group = ObjectFactory.build_group(group.group_name, group.group_id)
-    m_user = ObjectFactory.build_user(users[0].name, users[0].name + ' Ivanov', users[0].telegram_id)
-    
-    assert AdminUnit.check_admin(m_user.telegram_id, m_group.telegram_id) is False
+    m_user = ObjectFactory.build_user(
+            users[0].name,
+            users[0].name + ' Ivanov',
+            users[0].telegram_id)
+
+    assert AdminUnit.check_admin(
+            m_user.telegram_id, m_group.telegram_id) is False
 
 
 def test_is_photo_registered(create_user, group, db):
@@ -164,12 +184,17 @@ def test_is_photo_registered(create_user, group, db):
     m_group = ObjectFactory.build_group(group.group_name, group.group_id)
 
     for user in users:
-        register_unit.register_photo_for_contest(user.telegram_id, m_group.telegram_id)
-    all_photo_ids = register_unit.select_contest_photos_ids(m_group.telegram_id)
+        register_unit.register_photo_for_contest(
+                user.telegram_id, m_group.telegram_id
+                )
+    all_photo_ids = register_unit.select_contest_photos_ids(
+            m_group.telegram_id
+            )
     assert len(all_photo_ids) == 5
 
 
-def test_is_photo_registered_without_duplicating_submissions(create_user, group, db):
+def test_is_photo_registered_without_duplicating_submissions(create_user,
+                                                             group, db):
     users: list[User] = []
     for user in range(0, 5):
         users.append(create_user())
@@ -177,16 +202,20 @@ def test_is_photo_registered_without_duplicating_submissions(create_user, group,
     m_group = ObjectFactory.build_group(group.group_name, group.group_id)
 
     for user in users:
-        register_unit.register_photo_for_contest(user.telegram_id, m_group.telegram_id)
-        register_unit.register_photo_for_contest(user.telegram_id, m_group.telegram_id)
-    all_photo_ids = register_unit.select_contest_photos_ids(m_group.telegram_id)
+        register_unit.register_photo_for_contest(user.telegram_id,
+                                                 m_group.telegram_id)
+        register_unit.register_photo_for_contest(user.telegram_id,
+                                                 m_group.telegram_id)
+    all_photo_ids = register_unit.select_contest_photos_ids(
+            m_group.telegram_id
+            )
     assert len(all_photo_ids) == 5
 
 
 def test_is_vote_not_started(create_user, group, db):
     AdminUnit = AdminDB(db)
     m_group = ObjectFactory.build_group(group.group_name, group.group_id)
-    assert AdminUnit.get_current_vote_status(m_group.telegram_id) == False
+    assert AdminUnit.get_current_vote_status(m_group.telegram_id) is False
 
 
 def test_is_vote_started(create_user, group, db):
@@ -194,7 +223,8 @@ def test_is_vote_started(create_user, group, db):
     m_group = ObjectFactory.build_group(group.group_name, group.group_id)
     AdminUnit.change_current_vote_status(m_group.telegram_id)
 
-    assert AdminUnit.get_current_vote_status(m_group.telegram_id) == True
+    assert AdminUnit.get_current_vote_status(m_group.telegram_id) is True
+
 
 def test_is_vote_changed_again(create_user, group, db):
     AdminUnit = AdminDB(db)
@@ -205,6 +235,7 @@ def test_is_vote_changed_again(create_user, group, db):
     new_res = AdminUnit.get_current_vote_status(m_group.telegram_id)
 
     assert cur_res == new_res
+
 
 def test_is_vote_finished_correctly(create_user, group, db):
     users: list[User] = []
@@ -218,8 +249,13 @@ def test_is_vote_finished_correctly(create_user, group, db):
 
     for user in users:
         file_id = random.randint(0, 100000)
-        register_unit.register_photo_for_contest(user.telegram_id, m_group.telegram_id, file_get_id=str(file_id))
-    all_photo_ids = register_unit.select_contest_photos_ids(m_group.telegram_id)
+        register_unit.register_photo_for_contest(
+                user.telegram_id,
+                m_group.telegram_id,
+                file_get_id=str(file_id))
+    all_photo_ids = register_unit.select_contest_photos_ids(
+            m_group.telegram_id
+            )
     like.like_photo_with_file_id(users[0].telegram_id, all_photo_ids[0])
     like.like_photo_with_file_id(users[0].telegram_id, all_photo_ids[1])
     like.like_photo_with_file_id(users[0].telegram_id, all_photo_ids[2])
@@ -230,7 +266,7 @@ def test_is_vote_finished_correctly(create_user, group, db):
     like.insert_all_likes(users[2].telegram_id, m_group.telegram_id)
 
     photo_winner = vote.select_winner_from_contest(m_group.telegram_id)
-    assert photo_winner == 1 
+    assert photo_winner == 1
 
 
 def test_is_vote_finished_correctly_second(create_user, group, db):
@@ -245,8 +281,13 @@ def test_is_vote_finished_correctly_second(create_user, group, db):
 
     for user in users:
         file_id = random.randint(0, 100000)
-        register_unit.register_photo_for_contest(user.telegram_id, m_group.telegram_id, file_get_id=str(file_id))
-    all_photo_ids = register_unit.select_contest_photos_ids(m_group.telegram_id)
+        register_unit.register_photo_for_contest(
+                user.telegram_id,
+                m_group.telegram_id,
+                file_get_id=str(file_id))
+    all_photo_ids = register_unit.select_contest_photos_ids(
+            m_group.telegram_id
+            )
     like.like_photo_with_file_id(users[0].telegram_id, all_photo_ids[0])
     like.like_photo_with_file_id(users[0].telegram_id, all_photo_ids[1])
     like.like_photo_with_file_id(users[0].telegram_id, all_photo_ids[2])
@@ -257,7 +298,7 @@ def test_is_vote_finished_correctly_second(create_user, group, db):
     like.insert_all_likes(users[2].telegram_id, m_group.telegram_id)
 
     photo_winner = vote.select_winner_from_contest(m_group.telegram_id)
-    assert photo_winner == 3 
+    assert photo_winner == 3
 
 
 def test_is_likes_correctly_counted(create_user, group, db):
@@ -272,8 +313,13 @@ def test_is_likes_correctly_counted(create_user, group, db):
 
     for user in users:
         file_id = random.randint(0, 100000)
-        register_unit.register_photo_for_contest(user.telegram_id, m_group.telegram_id, file_get_id=str(file_id))
-    all_photo_ids = register_unit.select_contest_photos_ids(m_group.telegram_id)
+        register_unit.register_photo_for_contest(
+                user.telegram_id,
+                m_group.telegram_id,
+                file_get_id=str(file_id))
+    all_photo_ids = register_unit.select_contest_photos_ids(
+            m_group.telegram_id
+            )
     like.like_photo_with_file_id(users[0].telegram_id, all_photo_ids[0])
     like.like_photo_with_file_id(users[0].telegram_id, all_photo_ids[1])
     like.like_photo_with_file_id(users[0].telegram_id, all_photo_ids[2])
@@ -282,7 +328,9 @@ def test_is_likes_correctly_counted(create_user, group, db):
     like.insert_all_likes(users[0].telegram_id, m_group.telegram_id)
     like.insert_all_likes(users[1].telegram_id, m_group.telegram_id)
     like.insert_all_likes(users[2].telegram_id, m_group.telegram_id)
-    all_photo_ids = register_unit.select_contest_photos_primary_ids(m_group.telegram_id)
+    all_photo_ids = register_unit.select_contest_photos_primary_ids(
+            m_group.telegram_id
+            )
 
     list_of_likes = [1, 1, 3, 0, 0]
     i = 0
@@ -306,8 +354,13 @@ def test_is_likes_correctly_counted_file_id(create_user, group, db):
 
     for user in users:
         file_id = random.randint(0, 100000)
-        register_unit.register_photo_for_contest(user.telegram_id, m_group.telegram_id, file_get_id=str(file_id))
-    all_photo_ids = register_unit.select_contest_photos_ids(m_group.telegram_id)
+        register_unit.register_photo_for_contest(
+                user.telegram_id,
+                m_group.telegram_id,
+                file_get_id=str(file_id))
+    all_photo_ids = register_unit.select_contest_photos_ids(
+            m_group.telegram_id
+            )
     like.like_photo_with_file_id(users[0].telegram_id, all_photo_ids[0])
     like.like_photo_with_file_id(users[0].telegram_id, all_photo_ids[1])
     like.like_photo_with_file_id(users[0].telegram_id, all_photo_ids[2])
@@ -325,4 +378,3 @@ def test_is_likes_correctly_counted_file_id(create_user, group, db):
             photo_like = 0
         assert photo_like == list_of_likes[i]
         i += 1
-
