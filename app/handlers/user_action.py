bd62319b9c99ee44df6304d1853d99e89aff88ba
Nikita Baranov <baranovns@gmail.com>
2023-07-30 21:29:28 +0300

    refactor participants and leader boards

--- app/handlers/user_action.py
+++ app/handlers/user_action.py
@@ -1,5 +1,5 @@
 from aiogram import types
-from aiogram.utils.markdown import link, hlink
+from aiogram.utils.markdown import hlink
 from db.db_operations import RegisterDB
 from handlers.internal_logic.register import internal_register_photo
 from utils.TelegramUserClass import (
@@ -74,30 +74,37 @@ async def is_valid_input(
         return True
 
 
+def generate_board_message(template, user_list):
+    txt = ''
+    if not user_list:
+        txt = "Пока нет данных."
+    for place, user_data in enumerate(user_list, start=1):
+        user, total = user_data
+        txt += template.format(
+            place=place, link=hlink(user, f'https://t.me/{user}'), total=total,
+        )
+    return txt
+
+
 async def view_leaders(message: types.Message, register_unit: RegisterDB):
     if message.chat.type == "private":
         return
     leader_list = await register_unit.select_winner_leaderboard(message.chat.id)
-    txt = ''
-    if not leader_list:
-        txt = "Пока нет данных"
-    for place, leader_data in enumerate(leader_list, start=1):
-        leader, total_wins = leader_data
-        txt += f"<b>{place}</b>: {hlink(leader, f'https://t.me/{leader}')}, количество побед: {total_wins}\n"
-    await message.reply(txt, parse_mode="HTML", disable_web_page_preview=True)
+    template = "<b>{place}</b>: {link}, количество побед: {total}\n"
+    await message.reply(
+        generate_board_message(template, leader_list),
+        parse_mode="HTML",
+        disable_web_page_preview=True,
+    )
 
 
 async def view_overall_participants(message: types.Message, register_unit: RegisterDB):
     if message.chat.type == "private":
         return
-    leader_list = await register_unit.select_participants_table(message.chat.id)
-    txt = ""
-    i = 1
-    for leader in leader_list:
-        txt += (
-            "<b>{i}:</b> @{leader[0]} и количество участий в челлендже: {leader[1]}\n".format()
-        )
-        i += 1
-    if i == 1:
-        txt = "Пока нет данных"
-    await message.reply(txt, parse_mode="HTML")
+    participants_list = await register_unit.select_participants_table(message.chat.id)
+    template = "<b>{place}</b>: {link}, количество участий в челлендже {total}\n"
+    await message.reply(
+        generate_board_message(template, participants_list),
+        parse_mode="HTML",
+        disable_web_page_preview=True,
+    )
